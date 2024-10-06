4479a7c0b9118d8e330c40c66779b78429068285
Kirill0170 <kirill17.rzd@icloud.com>
2024-10-06 20:54:18 +0300

    initial world commit

---
+++ core/server.py
@@ -0,0 +1,6 @@
+import socket
+import core.log as log
+def server():
+    pass
+def startServer(world,address):
+    ip,port=address
---
+++ core/world/worldgen.py
@@ -0,0 +1,3 @@
+def generateSimpleWorld():
+    blocks=[]
+    return blocks
